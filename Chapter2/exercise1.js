3ef4fda11f5bf8f30fe6c20fa5ef69cad01eb029
weijingning <weijingning@360.cn>
2015-04-30 18:07:25 +0800

    add question

--- Chapter2/exercise1.js
+++ Chapter2/exercise1.js
@@ -1,3 +1,15 @@
+Looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
 for (var i = 0; i < 7; i++) {
 	var lineContent = "";
 	for (var j = 0; j <= i; j++) {
