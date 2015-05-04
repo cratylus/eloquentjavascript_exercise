1f11f323cb6c7407d0fc746fb4b7b3dbecf90e88
cratylus <pi31415926532@gmail.com>
2015-05-04 15:42:24 +0800

    exercise2 in chapter 4

---
+++ Chapter4/sumOfRange.js
@@ -0,0 +1,28 @@
+var range = function(start,end){
+  var step = arguments[2]||1;
+  console.log("step:",step);
+  var result = [];
+  if(step>0){
+    for(;start<=end;start+=step){
+      result.push(start);
+    }
+  }else{
+    for(;start>=end;start+=step){
+      result.push(start);
+    }
+  }
+  return result;
+}
+
+var sum = function(list){
+  var sum = 0;
+  for(var i=0;i<list.length;i++){
+    sum+=list[i];
+  }
+  return sum;
+}
+    
+console.log(sum(range(1, 10)));
+// → 55
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
