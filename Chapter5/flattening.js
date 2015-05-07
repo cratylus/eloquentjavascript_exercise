bf45629773e4b4d69ddfb988850c03cf26231d28
cratylus <pi31415926532@gmail.com>
2015-05-07 20:10:32 +0800

    exercise in chapter5

---
+++ Chapter5/flattening.js
@@ -0,0 +1,12 @@
+Flattening
+
+Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the input arrays.
+
+var arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+// → [1, 2, 3, 4, 5, 6]
+var flattenArray = arrays.reduce(function(result,current){
+	return result.concat(current);
+});
+
+console.log(flattenArray);
---
+++ Chapter5/historicalLifeExpectancy.js
@@ -0,0 +1,27 @@
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+function groupBy(array,groupOf){
+	var groups = {};
+	array.forEach(function(element){
+		var groupName = groupOf(element);
+		if(groupName in groups){
+			groups[groupName].push(element);
+		}else{
+			groups[groups] = [element];
+		}
+	});
+	return groups;
+}
+
+var byCentury = groupBy(ancestry,function(person){
+	return Math.ceil(person.died / 100);
+});
+
+for(var century in byCentury){
+	var ages = byCentury[century].map(function(person){
+		return person.died - person.born;
+	});
+}
